b0a8ffcfb01279bae9c4a9d6c208d6e747094ad5
ldhwaddell <ldhwaddell@gmail.com>
2023-11-16 20:27:02 -0400

    remove optional reponse on CheckValidUrlResponse

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -22,7 +22,7 @@ const Popup = () => {
       return new Promise((resolve, reject) => {
         chrome.runtime.sendMessage(
           message,
-          (response?: CheckValidUrlResponse) => {
+          (response: CheckValidUrlResponse) => {
             if (chrome.runtime.lastError) {
               reject(chrome.runtime.lastError);
             }
