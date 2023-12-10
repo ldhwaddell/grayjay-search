f711e9f22a1ca5489998ac4d223be91ab541cea7
ldhwaddell <ldhwaddell@gmail.com>
2023-12-10 00:08:55 -0400

    add query change msg type

--- src/types/messages.ts
+++ src/types/messages.ts
@@ -1,3 +1,5 @@
+import { Query } from "./query";
+
 export type ErrorScrapingLinksMessage = {
   type: "ERROR_SCRAPING_LINKS";
 };
@@ -7,3 +9,7 @@ export type ProcessLinksMessage = {
   links: string[];
 };
 
+export type QueryChangeMessage = {
+  type: "QUERY_CHANGE";
+  query: Query;
+};
