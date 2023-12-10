0b0961f59982fffed79aeafb4552d82468b630e6
ldhwaddell <ldhwaddell@gmail.com>
2023-12-10 00:08:25 -0400

    add getCurrentTab function

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,12 @@ const regex: RegExp =
   /^https?:\/\/(?:www\.)?grayjayleagues\.com\/.*[?&]all_games=1(&|$).*/;
 
 export const isValidUrl = (url: string): boolean => regex.test(url);
+
+export const getCurrentTab = async (): Promise<chrome.tabs.Tab> => {
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+
+  return tab;
+};
