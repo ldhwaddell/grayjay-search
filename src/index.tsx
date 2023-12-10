3cd0a3552722d98a943a337c2851d7797875313a
ldhwaddell <ldhwaddell@gmail.com>
2023-12-09 21:48:59 -0400

    remove unnecessary styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import Popup from "./Popup";
 import "./Popup.css";
 
 const root = document.createElement("div");
-root.className = "container";
 document.body.appendChild(root);
 const rootDiv = ReactDOM.createRoot(root);
 
