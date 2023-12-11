58f012510da7e38a41321724cb7baf1c67c74b34
ldhwaddell <ldhwaddell@gmail.com>
2023-12-10 23:02:45 -0400

    move query check to content script

--- src/Pages/SearchPage/SearchPage.tsx
+++ src/Pages/SearchPage/SearchPage.tsx
@@ -5,7 +5,7 @@ import RadioButton from "../../Components/RadioButton/RadioButton";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 import Toggle from "../../Components/Toggle/Toggle";
 
-import { getCurrentTab, isQueryNull } from "../../utils";
+import { getCurrentTab } from "../../utils";
 
 import "./SearchPage.css";
 
@@ -83,9 +83,6 @@ const SearchPage = ({ games }: Props) => {
         chrome.tabs.sendMessage(tab.id, message);
       }
     };
-
-    // Don't send message if fields are null
-    if (isQueryNull(query)) return;
 
     sendMessage(query);
   }, [query]);
