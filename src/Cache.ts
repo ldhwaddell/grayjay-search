2eccfbbb4a19139ffee061230b4e326e2eb8247e
ldhwaddell <ldhwaddell@gmail.com>
2023-12-17 17:32:17 -0400

    simplify cache logic

--- src/Cache.ts
+++ src/Cache.ts
@@ -1,38 +1,24 @@
-import { GameData, Query } from "./types";
+import { GameData } from "./types";
 
 const GAME_CACHE_KEY: string = "games";
-const QUERY_CACHE_KEY: string = "query";
 
 export class Cache {
-  static get = async (key: string = GAME_CACHE_KEY): Promise<GameData[]> => {
+  static get = async (key: string): Promise<any> => {
     return new Promise((resolve, reject) => {
       chrome.storage.local.get([key], (result) => {
         if (chrome.runtime.lastError) {
           reject(new Error(chrome.runtime.lastError.message));
         } else {
-          const data: GameData[] = result[key] ?? [];
+          const data = result[key] ?? null;
           resolve(data);
         }
       });
     });
   };
 
-  static getQuery = async (key: string = QUERY_CACHE_KEY): Promise<Query> => {
+  static update = async (key: string, data: any): Promise<void> => {
     return new Promise((resolve, reject) => {
-      chrome.storage.local.get([key], (result) => {
-        if (chrome.runtime.lastError) {
-          reject(new Error(chrome.runtime.lastError.message));
-        } else {
-          const data: Query = result[key] ?? {};
-          resolve(data);
-        }
-      });
-    });
-  };
-
-  static updateQuery = async (newQuery: Query): Promise<void> => {
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.set({ [QUERY_CACHE_KEY]: newQuery }, () => {
+      chrome.storage.local.set({ [key]: data }, () => {
         if (chrome.runtime.lastError) {
           reject(new Error(chrome.runtime.lastError.message));
         } else {
@@ -42,48 +28,23 @@ export class Cache {
     });
   };
 
-  static add = async (gameData: GameData[]): Promise<void> => {
-    if (!gameData || !gameData.length) {
-      return;
-    }
-
-    const currentGames: GameData[] = await this.get();
-
+  static addGames = async (gameData: GameData[]): Promise<void> => {
+    const currentGames: GameData[] = (await this.get(GAME_CACHE_KEY)) || [];
     const updatedGames: GameData[] = [...currentGames, ...gameData];
 
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.set({ [GAME_CACHE_KEY]: updatedGames }, () => {
-        if (chrome.runtime.lastError) {
-          reject(new Error(chrome.runtime.lastError.message));
-        }
-        resolve();
-      });
-    });
+    await this.update(GAME_CACHE_KEY, updatedGames);
   };
 
-  // // Make sure it handles case of adding/removing at same time
-  static remove = async (gamesToRemove: number[]): Promise<void> => {
-    // Return early if there are no games to remove
-    if (!gamesToRemove || !gamesToRemove.length) {
-      return;
-    }
-
+  // Make sure it handles case of adding/removing at same time
+  static removeGames = async (gamesToRemove: number[]): Promise<void> => {
     // Fetch the current games from the cache
-    const currentGames: GameData[] = await this.get();
+    const currentGames: GameData[] = await this.get(GAME_CACHE_KEY);
 
     // Filter out the games that need to be removed
     const updatedGames: GameData[] = currentGames.filter(
       (game) => !gamesToRemove.includes(game.id)
     );
 
-    // Update the cache with the filtered list
-    return new Promise((resolve, reject) => {
-      chrome.storage.local.set({ [GAME_CACHE_KEY]: updatedGames }, () => {
-        if (chrome.runtime.lastError) {
-          reject(new Error(chrome.runtime.lastError.message));
-        }
-        resolve();
-      });
-    });
+    await this.update(GAME_CACHE_KEY, updatedGames);
   };
 }
